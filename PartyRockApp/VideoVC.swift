b1958a67597b9a0bade6788287842af7a0be0249
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-04 19:49:43 +0800

    prototypes app design and views

---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,16 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by John Crisostomo on 04/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
