70dd63dcfa243602efc68c1830db78a300fb3866
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-04 19:09:06 +0800

    Initial commit

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by John Crisostomo on 04/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
